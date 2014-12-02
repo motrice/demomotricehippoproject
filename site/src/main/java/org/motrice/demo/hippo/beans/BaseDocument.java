c35ad6cdd2128c6eafcda395eeeaff550f2530e3
Björn Molin <bjorn.molin@inherit.se>
2014-12-02 13:49:02 +0100

    initial generated hippo project

---
+++ site/src/main/java/org/motrice/demo/hippo/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package org.motrice.demo.hippo.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="demomotricehippoproject:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
